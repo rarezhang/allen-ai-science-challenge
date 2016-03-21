eb889b267d53d65cf2f9bebc26ba193141731aa5
wenlizhang <catezhang@qq.com>
2016-03-20 17:34:58 -0700

    add question id

--- src/question_answer_analysis.py
+++ src/question_answer_analysis.py
@@ -19,12 +19,12 @@ def read_kaggle_file(path='', training_set_flag=True, sep='\t'):
     :param sep: separator
     :return:
     """
-    qid, questions, correctAnswer, answers = [], [], [], []
+    question_id, questions, correctAnswer, answers = [], [], [], []
     with io.open(path, 'r', encoding='utf-8', errors='ignore') as f:
         next(f)  # ignore first line
         for line in f:
             line = line.strip().split(sep)
-            # qid.append(line[0])  # question id
+            question_id.append(line[0])  # question id
             questions.append(remove_punctuation(line[1].lower()))  # question text   #lower():because lucene cannot parse <NOT>
             if training_set_flag:
                 correctAnswer.append(line[2])  # correct answer A-D
@@ -32,9 +32,9 @@ def read_kaggle_file(path='', training_set_flag=True, sep='\t'):
             else:
                 answers.append(line[2:])
     if training_set_flag:
-        return questions, correctAnswer, answers
+        return question_id, questions, correctAnswer, answers
     else:
-        return questions, answers
+        return question_id, questions, answers
 
 
 @load_or_make
